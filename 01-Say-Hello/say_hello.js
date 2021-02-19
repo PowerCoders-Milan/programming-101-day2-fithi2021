2ae77dd68c09f15f0eab544231562329430b8b03
fithawi ghide <lgfetah@gmail.com>
2021-02-19 11:32:40 +0100

    Say-Hello Exercise

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,8 +1,15 @@
 //insert your pseudocode below
-/*
+/*create a variable to store the name
+*create a variable to store the greeting
+*and ask the user to enter his name Example"Enter your name"
+*print(name+greeting)
 
-here
+*
 
 */
 
 //insert your code below
+var name;
+var greeting = "Salve"
+var name = window.prompt("Insrisci il tuo nome: ");
+alert(greeting + name);
