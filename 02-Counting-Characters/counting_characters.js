910bdf50837d7ed0559da20a655dcdf10223d3ee
fithawi ghide <lgfetah@gmail.com>
2021-02-19 11:47:31 +0100

    Counting char Exe

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,16 @@
 //insert your pseudocode below
-/*
+/* create a variable to store the String
+*and ask a user to enter string only we can with some constraints
+* una altra variabile che memorizza il legth of the String che ha
+*inserito il utente
+*visualizzi il che ha inserito e the legth.
 
 here
 
 */
 
 //insert your code below
+var some_String = window.prompt("Hey, Inserisci qual che stringa");
+var legth_of_String = some_String.length;
+console.log(some_String );
+console.log(some_String );
