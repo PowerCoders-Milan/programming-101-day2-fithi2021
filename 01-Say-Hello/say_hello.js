cbe12937140781dec98173f787403222b6695f73
fithawi ghide <lgfetah@gmail.com>
2021-02-19 12:23:09 +0100

    Second commit from say_hello

--- 01-Say-Hello/say_hello.js
+++ 01-Say-Hello/say_hello.js
@@ -1,15 +1,16 @@
 //insert your pseudocode below
-/*create a variable to store the name
+/*create a variable to store the yourname
 *create a variable to store the greeting
 *and ask the user to enter his name Example"Enter your name"
-*print(name+greeting)
+*print(yourname+greeting)
 
 *
 
 */
 
 //insert your code below
-var name;
-var greeting = "Salve"
-var name = window.prompt("Insrisci il tuo nome: ");
-alert(greeting + name);
+var greeting, name2;
+greeting = 'Hello ';
+name2 = window.prompt('Enter your name');
+window.alert(greeting + name2);
+
